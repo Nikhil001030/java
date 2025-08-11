eb3fa4028674cfdfddb6fc386b8f40ba21b1d6bc
Nikhil Pal <nikhilpal630622@gmail.com>
2025-08-11 11:04:13 +0530

    Add files via upload
    Sum of numbers using local variable

---
+++ Lecture.java
@@ -0,0 +1,17 @@
+public class Lecture {
+    public static void main(String[] args) {
+        int a=3;//local variable
+        int _b=857; 
+        int $c=45;
+        int nameNikhil=5345;
+        //System.out.println(a);
+        //System.out.println(_b);
+        //System.out.println($c);
+        int sum=a+_b+$c+nameNikhil;
+        System.out.println(sum);
+    }
+}
+
+
+
+
